747c27d0b859e8bcdca751394baa5a2b3079517d
Giarlan Santana <giarlansilva@yahoo.com>
2023-06-06 19:35:44 -0300

    COMPLETE THE PROJECT
       *Build - The project has been completed after creating the JS script for automating Typing in the 'Home' section

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+/* =========================== Typing Animation ========================== */
+let typed = new Typed('.typing',{
+    strings:['', ' nail and eyebrow designer, and I do eyelash extensions.'],
+    backSpeed: 20,
+    typeSpeed: 80,
+    loop: true,
+    shuffle: true
+  });
+
+
+
+
